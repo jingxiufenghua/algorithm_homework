1581c3f8e696cd52c98c3045de72c5baea7a4d95
git <2450392436@qq.com>
2021-02-20 14:27:03 +0800

    leetcode46.py

---
+++ leetcode687.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+
+    def longestUnivaluePath(self, root: TreeNode) -> int:
+        self.ans = 0
+        def longestUnivalue(root: TreeNode) -> int:
+            if not root:return 0
+            left_length =  longestUnivalue(root.left)
+            right_length =  longestUnivalue(root.right)
+            left_arrow = right_arrow= 0
+            if root.left and root.left.val == root.val:
+                left_arrow = left_length + 1
+            if root.right and root.right.val == root.val:
+                right_arrow = right_length + 1
+            self.ans = max(self.ans,left_arrow+right_arrow)
+            return max(left_arrow,right_arrow)
+        longestUnivalue(root)
+        return self.ans
+nums = [1,4,5,4,4,5]
