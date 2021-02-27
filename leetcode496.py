3c2d636af8a9deb5d52d09ff197e24f0b552fabe
git <2450392436@qq.com>
2021-02-27 09:17:16 +0800

    leetcode84&&496.py

---
+++ leetcode496.py
@@ -0,0 +1,39 @@
+from typing import List
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        len1,len2 = len(nums1),len(nums2)
+        res_list = []
+        for i in range(len1):
+            local = nums2.index(nums1[i])
+            res = -1
+            while local<len2:
+                if nums1[i]<nums2[local]:
+                    res = local
+                    break
+                else:
+                    local+=1
+            if res<0:
+                res_list.append(res)
+            else:
+                res_list.append(nums2[res])
+        return res_list
+# 单调递减栈
+    def nextGreaterElement1(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack,result_dict= [],{}
+        len1,len2 = len(nums1),len(nums2)
+        for i in range(len2):
+            while stack and stack[-1]<nums2[i]:
+                result_dict[stack.pop()] = nums2[i]
+            stack.append(nums2[i])
+        result_dict[nums2[len2-1]] = -1
+        return [result_dict.get(x,-1) for x in nums1]
+
+
+solution = Solution()
+nums1 = [4,1,2]
+nums2 = [1,3,4,2]
+# result = solution.nextGreaterElement(nums1,nums2)
+# nums1 = [2,4]
+# nums2 = [1,2,3,4]
+result = solution.nextGreaterElement1(nums1,nums2)
+print(result)
--- leetcode84.py
+++ leetcode84.py
@@ -1,3 +1,4 @@
+# 超出时间限制
 from  typing import  List
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
@@ -11,8 +12,36 @@ class Solution:
                 right+=1
             res = max(res,(right-left-1)*heights[i])
         return res
+
+    def largestRectangleArea1(self, heights: List[int]) -> int:
+        n = len(heights)
+        left,right = [0]*n,{0}*n
+
+        mono_list = list()
+        for i in range(n):
+            while mono_list and heights[mono_list[-1]]>=heights[i]:
+                mono_list.pop()
+            left[i] = mono_list[-1] if mono_list else -1
+            mono_list.append(i)
+
+        mono_list = list()
+        for i in range(n-1,-1,-1):
+            while mono_list and heights[mono_list[-1]] >= heights[i]:
+                mono_list.pop()
+            right[i] = mono_list[-1] if mono_list else n
+            mono_list.append(i)
+        ans = max((right[i]-left[i]-1)*heights[i] for i in range(n))  if n>0 else 0
+        return ans
+
+
+
+
+
+
 solution = Solution()
 nums = [2,1,5,6,2,3]
 # nums = [2,4]
 result = solution.largestRectangleArea(heights=nums)
+result1 = solution.largestRectangleArea1(heights=nums)
 print(result)
+print(result1)
