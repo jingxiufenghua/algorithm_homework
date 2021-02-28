c27a74d83b0d21f46c31d4882c07769e9692dab4
git <2450392436@qq.com>
2021-02-28 18:13:58 +0800

    leetcode739.py

---
+++ leetcode739.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        n = len(T)
+        stack = []
+        res_dict = {}
+        for i in range(n):
+            while stack and T[stack[-1]]<T[i]:
+                res_dict[stack[-1]]=i-stack[-1]
+                stack.pop()
+            stack.append(i)
+        return [res_dict.get(x,0) for x in range(n)]
+solution = Solution()
+temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
+result = solution.dailyTemperatures(temperatures)
+print(result)
