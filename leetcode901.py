3f6f0678ae27ab336d3c34f8db72b7cb65eddc7a
git <2450392436@qq.com>
2021-03-01 23:28:11 +0800

    leetcode79.py

--- leetcode901.py
+++ leetcode901.py
@@ -1,10 +1,31 @@
-class StockSpanner:
 
+class StockSpanner(object):
     def __init__(self):
+        self.stack = []
 
-
-    def next(self, price: int) -> int:
+    def next(self, price):
+        weight = 1
+        while self.stack and self.stack[-1][0] <= price:
+            weight += self.stack.pop()[1]
+        self.stack.append((price, weight))
+        return weight
 
 # Your StockSpanner object will be instantiated and called as such:
-# obj = StockSpanner()
-# param_1 = obj.next(price)
+S= StockSpanner()
+param_1 = S.next(100)
+print(param_1)
+param_1 = S.next(80)
+print(param_1)
+param_1 = S.next(60)
+print(param_1)
+param_1 = S.next(70)
+print(param_1)
+param_1 = S.next(60)
+print(param_1)
+param_1 = S.next(75)
+print(param_1)
+param_1 = S.next(85)
+print(param_1)
+
+
+
