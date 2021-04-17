691a45997e082ff9344330753eaafb391391a7f0
git <2450392436@qq.com>
2021-04-17 16:10:37 +0800

    move

---
+++ leetcode/leetcode1438.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：algorithm_homework 
+@File    ：leetcode1438.py
+@IDE     ：PyCharm 
+@Author  ：无名
+@Date    ：2021/4/17 15:42 
+'''
+# 1438. 绝对差不超过限制的最长连续子数组
+from typing import List
+
--- leetcode/leetcode220.py
+++ leetcode/leetcode220.py
@@ -11,19 +11,8 @@
 from typing import List
 class Solution:
     def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
-        if not nums: return False
-        temp = nums[:]
-        nums.sort()
-        i,j = 0,len(nums)-1
-        while i<=j:
-            if nums[j]-nums[i]<=t:
-                break
-            j -= 1
-        list_i = [i  for i,x in enumerate(nums) if x == nums[i]]
-        list_j = [j  for j,x in enumerate(nums) if x == nums[j]]
-        for (i,j) in zip(list_i,list_j):
-            print(i,j)
-        return False
+
+
 solution = Solution()
 nums = [1,2,3,1]
 result = solution.containsNearbyAlmostDuplicate(nums,3,0)
