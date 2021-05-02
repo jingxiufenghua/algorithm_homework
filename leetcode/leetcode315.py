a3736ede139d2be12a97018ee3da7283464aa23a
git <2450392436@qq.com>
2021-05-02 23:27:51 +0800

    5.2

---
+++ leetcode/leetcode315.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：algorithm_homework 
+@File    ：leetcode315.py
+@IDE     ：PyCharm 
+@Author  ：无名
+@Date    ：2021/5/2 23:27 
+'''
+from typing import List
